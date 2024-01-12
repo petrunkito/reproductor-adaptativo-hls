1cf11169be3576faf18f630d150b0dff3be40d04
aurelioobando <aureliosflow@gmail.com>
2024-01-11 18:11:49 -0600

    agregar: agregamos comentarios explicando el codigo del programa y agregando una nota de aclaracion(Nota!)

--- api/utilities/createFiles.js
+++ api/utilities/createFiles.js
@@ -4,15 +4,22 @@
  *  -temp:aqui se almacena de manera temporal los videos del usuario que luego seran fragmentados
  */
 
+//?para trabajar con el sistema de archivos
 const fs = require("fs/promises")
+//?mis atajos de rutas 
 const paths = require("../paths")
+//?para el manejo de rutas
 const path = require("path")
 
 async function init(){
     try{
+        //!Nota: si las carpetas ya existen, este no arroja ningun error y no sobreescribe ningun archivo
+        //?creamos nuestras carpetas asi: /uploads/temp en la raiz del proyecto
         let resultado = await fs.mkdir(path.join(paths.index, "uploads", "temp"), {recursive:true})
+        //?si las carpetas se crearon imprimimos este mensaje
         if(resultado) console.log("directories created successfully")
     }catch(err){
+        //?en caso de error, imprimimos el mensaje
         console.log("createFiles.js error: ", err)
     }
 }
